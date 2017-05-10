be579e84fd62916d00fd04728db0a639333676d5
Anish Chadalavada <anish.chada@gmail.com>
2017-05-10 15:15:21 -0400

    have the getpost method working now

--- app/controllers/post_controller.js
+++ app/controllers/post_controller.js
@@ -3,12 +3,20 @@ import Post from '../models/post_model';
 // The code for the createPost method was given to use by Tim in the assignment
 // I used the Mongoose methods documentation to implement these controller methods
 
+// This cleanposts method was given to us by Tim in the assignment
+
+const cleanPosts = (posts) => {
+  return posts.map((post) => {
+    return { id: post._id, title: post.title, tags: post.tags, cover_url: post.cover_url };
+  });
+};
+
 export const createPost = (req, res) => {
   const post = new Post();
   post.title = req.body.title;
   post.tags = req.body.tags;
   post.content = req.body.content;
-  post.cover_url = req.cover_url;
+  post.cover_url = req.body.cover_url;
   post.save()
     .then((result) => {
       res.json({ message: 'Post created!' });
@@ -34,7 +42,8 @@ export const getPosts = (req, res) => {
 export const getPost = (req, res) => {
   Post.findById(req.params.id)
     .then((result) => {
-      res.json({ result });
+      console.log(result);
+      res.json(result);
     })
     .catch((error) => {
       res.status(500).json({ error });
